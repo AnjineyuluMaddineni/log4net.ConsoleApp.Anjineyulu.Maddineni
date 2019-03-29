fb58a04ed406490b82e3cb856a2cc4c7f4b1e03e
Azure <Azure@Anji>
2019-03-29 13:12:47 +0000

    Add project files.

---
+++ log4net.ConsoleApp.Anjineyulu.Maddineni/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.ConsoleApp.Anjineyulu.Maddineni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
