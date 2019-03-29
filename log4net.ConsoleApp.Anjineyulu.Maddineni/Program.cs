d3082253c862dee861f4725b40cd17db2b1b4721
Azure <Anjineyulu.Maddineni@gmail.com>
2019-03-29 15:34:39 +0000

    commit 01 | Implemented log4net in Empty Console Application

--- log4net.ConsoleApp.Anjineyulu.Maddineni/Program.cs
+++ log4net.ConsoleApp.Anjineyulu.Maddineni/Program.cs
@@ -8,8 +8,26 @@ namespace log4net.ConsoleApp.Anjineyulu.Maddineni
 {
     class Program
     {
+       internal static void Testing()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ILog log = log4net.LogManager.GetLogger(typeof(Program));
+     
+            try
+            {
+                object o2 = null;
+                int i2 = (int)o2;   // Error
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error Message: " + ex.Message.ToString(), ex);
+            }
+            
+        }
         static void Main(string[] args)
         {
+            Testing();
+            Console.ReadLine();
         }
     }
 }
--- log4net.ConsoleApp.Anjineyulu.Maddineni/Properties/AssemblyInfo.cs
+++ log4net.ConsoleApp.Anjineyulu.Maddineni/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("log4net.ConsoleApp.Anjineyulu.Maddineni")]
+[assembly: AssemblyTitle("AssemblyInfo.cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -13,11 +13,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("36d27a6d-02d1-40eb-8de8-ec962a3aa9b0")]
